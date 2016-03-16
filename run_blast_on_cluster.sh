73ba7461c8122113a5cf29686a4dc1503f6d2e6d
Jason Stajich <jason@bioperl.org>
2016-03-16 15:09:15 -0700

    use shared folder

--- run_blast_on_cluster.sh
+++ run_blast_on_cluster.sh
@@ -6,5 +6,5 @@ cd $PBS_O_WORKDIR
 # comments here
 # load the module so you have BLAST tools in your path
 module load ncbi-blast
-DB=uniprot_sprot.fasta
+DB=/bigdata/stajichlab/shared/db/uniprot_sprot.fasta
 blastp -query bacillus_hit.fa -db $DB -evalue 1e-50 -out bacillus_sprot.BLASTP.tab -outfmt 7 -num_threads 4
